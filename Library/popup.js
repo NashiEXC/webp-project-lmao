b5dd7b21ea678a0c6b61213d62ae3cd84f742051
John Doe <Woomysquid99@gmail.com>
2024-07-11 12:39:49 +0800

    ew

---
+++ Library/popup.js
@@ -0,0 +1,254 @@
+news1.addEventListener(
+    "click",
+    function () {
+        news1content.classList.add(
+        "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == news1content) {
+            news1content.classList.remove(
+                "show"
+            );
+        }
+    }
+);		
+news2.addEventListener(
+    "click",
+    function () {
+        news2content.classList.add(
+        "show"
+        );
+    }
+);
+
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == news2content) {
+            news2content.classList.remove(
+                "show"
+            );
+        }
+    }
+);	
+news3.addEventListener(
+    "click",
+    function () {
+        news3content.classList.add(
+        "show"
+        );
+    }
+);
+
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == news3content) {
+            news3content.classList.remove(
+                "show"
+            );
+        }
+    }
+);	
+uninstp.addEventListener(
+    "click",
+    function () {
+        popupcontent420.classList.add(
+        "show"
+        );
+    }
+);
+canceluninstp.addEventListener(
+    "click",
+    function () {
+        popupcontent420.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == popupcontent420) {
+            popupcontent420.classList.remove(
+                "show"
+            );
+        }
+    }
+);	
+playbutttp.addEventListener(
+    "click",
+    function () {
+        popupcontent69.classList.add(
+        "show"
+        );
+    }
+);
+canceltp.addEventListener(
+    "click",
+    function () {
+        popupcontent69.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == popupcontent69) {
+            popupcontent69.classList.remove(
+                "show"
+            );
+        }
+    }
+);	
+playbuttpen.addEventListener(
+    "click",
+    function () {
+        popupcontent.classList.add(
+        "show"
+        );
+    }
+);
+cancel.addEventListener(
+    "click",
+    function () {
+        popupcontent.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == popupcontent) {
+            popupcontent.classList.remove(
+                "show"
+            );
+        }
+    }
+);
+playpd3.addEventListener(
+    "click",
+    function () {
+        popupcontent2.classList.add(
+        "show"
+        );
+    }
+);
+cancelpd3.addEventListener(
+    "click",
+    function () {
+        popupcontent2.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == popupcontent2) {
+            popupcontent2.classList.remove(
+                "show"
+            );
+        }
+    }
+);
+playdoom.addEventListener(
+    "click",
+    function () {
+        popupcontent3.classList.add(
+        "show"
+        );
+    }
+);
+canceld.addEventListener(
+    "click",
+    function () {
+        popupcontent3.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == popupcontent3) {
+            popupcontent3.classList.remove(
+                "show"
+            );
+        }
+    }
+);
+incs.addEventListener(
+    "click",
+    function () {
+        popupcontent4.classList.add(
+        "show"
+        );
+    }
+);
+yescs.addEventListener(
+    "click",
+    function () {
+        popupcontent4.classList.remove(
+            "show"
+        );
+    }
+);
+cancelcs.addEventListener(
+    "click",
+    function () {
+        popupcontent4.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == popupcontent4) {
+            popupcontent4.classList.remove(
+                "show"
+            );
+        }
+    }
+);
+inpd2.addEventListener(
+    "click",
+    function () {
+        popupcontent5.classList.add(
+        "show"
+        );
+    }
+);
+cancelpd2.addEventListener(
+    "click",
+    function () {
+        popupcontent5.classList.remove(
+            "show"
+        );
+    }
+);
+yespd2.addEventListener(
+    "click",
+    function () {
+        popupcontent5.classList.remove(
+            "show"
+        );
+    }
+);
+window.addEventListener(
+    "click",
+    function (event) {
+        if (event.target == popupcontent4) {
+            popupcontent5.classList.remove(
+                "show"
+            );
+        }
+    }
+);
