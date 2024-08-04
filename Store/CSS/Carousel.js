3a60d5a480a80018b655d0d084d2332c36287d27
KafkasSAHH <Lionel.limzs@gmail.com>
2024-08-04 15:39:12 +0800

    asd

--- Store/CSS/Carousel.js
+++ Store/CSS/Carousel.js
@@ -4,7 +4,22 @@ const images = [
     "/Store/Pictures/SSS2.png",
     "/Store/Pictures/SSS3.png",
     "/Store/Pictures/SSS4.png",
-    "/Store/Pictures/SSS5.png"
+    "/Store/Pictures/SSS5.png",
+    "/Store/Pictures/RSS1.png",
+    "/Store/Pictures/RSS2.png",
+    "/Store/Pictures/RSS3.png",
+    "/Store/Pictures/RSS4.png",
+    "/Store/Pictures/RSS5.png",
+    "/Store/Pictures/DSS1.png",
+    "/Store/Pictures/DSS2.png",
+    "/Store/Pictures/DSS3.png",
+    "/Store/Pictures/DSS4.png",
+    "/Store/Pictures/DSS5.png",
+    "/Store/Pictures/DRGSS1.png",
+    "/Store/Pictures/DRGSS2.png",
+    "/Store/Pictures/DRGSS3.png",
+    "/Store/Pictures/DRGSS4.png",
+    "/Store/Pictures/DRGSS5.png"
 ];
 const carousel = document.getElementById('Carousel');
 
